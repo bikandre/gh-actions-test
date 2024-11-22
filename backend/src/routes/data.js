949ed0b6e29caf2ab65b35eef48f1476ffee72e1
andrebik <bikai.andre@yahoo.com>
2024-11-22 16:08:27 -0500

    update

--- backend/src/routes/data.js
+++ backend/src/routes/data.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Sample data endpoint
 router.get('/', (req, res) => {
     res.json({
-        message: 'Hello from the backend! I am not saying that its running but its running. Remember when you wake up in the morning, you woke up. Eat what you can but do not eat what you cant',
+        message: 'I am not saying that its running but its running. Remember when you wake up in the morning, you woke up. Eat what you can but do not eat what you cant',
         data: [1, 2, 3, 4, 5],
     });
 });
