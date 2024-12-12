becf739f12add9436c66cef969e1338edb35a7ba
andrebik <bikai.andre@yahoo.com>
2024-12-11 23:47:50 -0500

    update

--- reactapp/src/services/api.js
+++ reactapp/src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 
-const API_URL = "http://app-ingress-check2-1182206137.us-east-1.elb.amazonaws.com/api/data";  // Internal service URL in Kubernetes
+const API_URL = "http://app-ingress-check2-1932067237.us-east-1.elb.amazonaws.com/api/data";  // Internal service URL in Kubernetes
 
 export const fetchData = async () => {
   try {
